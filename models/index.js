54d73d8bdeb904f42b4ed70a6b30175b336dbb48
Dorijan Grgić <dg50161@fer.hr>
2020-04-09 18:04:11 +0200

    Init project and skeleton

---
+++ models/index.js
@@ -0,0 +1,26 @@
+import { dbConfig } from "../config/dbConfig";
+import Sequelize from "sequelize";
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false,
+  logging: console.log,
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+/* 
+This is where we define our models
+*/
+
+export default db;
